4453e861a294beb97f29a67666eb4c931c4fdc70
Joseph <danielpaulboglo@gmail.com>
2024-03-14 23:29:17 -0400

    Added time-based custom message

--- src/main/java/org/example/WelcomeMessage.java
+++ src/main/java/org/example/WelcomeMessage.java
@@ -3,12 +3,10 @@ import java.time.LocalTime;
 
 public class WelcomeMessage {
     public String generateMessage(String name) {
-        String greeting;
-        if (LocalTime.now().isBefore(LocalTime.NOON)) {
-            greeting = "Good morning, " + name + ", Welcome to COMP367";
+        if (LocalTime.now().getHour() < 12) {
+            return "Good morning, " + name + ", Welcome to COMP367";
         } else {
-            greeting = "Good afternoon, " + name + ", Welcome to COMP367";
+            return "Good afternoon, " + name + ", Welcome to COMP367";
         }
-        return greeting;
     }
 }
